755f1921989bd63455c1e41bb7864dfb431bf8e7
Tomi Santoso <tomisantoso501@gmail.com>
2015-09-15 23:16:37 +0700

    Update Nyoba3.hs

--- Nyoba3.hs
+++ Nyoba3.hs
@@ -1,5 +1,6 @@
-# firsttry
-
+-- First Assignment
+-- Reimplement Haskell function
+-- DON'T USE GOOGLE
 module Nyoba3 where
 
 import Data.List
@@ -22,8 +23,8 @@ snd' (a,b) = b
 --pembatas
 
 min' a b
-  | a > b = a
-  | otherwise = b
+  | a > b = b
+  | otherwise = a
 
   --pembatas
 
@@ -33,6 +34,20 @@ max' a b
 
   --pembatas
 
-head' [x:xs] = x 
+head' (x:xs) = x
 
 --pembatas
+
+tail' (x:xs) = xs
+
+--pembatas
+
+drop' _ [] = []
+drop' 0 xs = xs
+drop' n (x:xs) = drop' (n-1) xs
+
+--pembatas
+
+take' x [] = []
+take' 0 (x:xs) = []
+take' a (x:xs) = x : take' (a-1) (xs)
